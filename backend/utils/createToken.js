52e9b09d12342f75f5a1f262072af2ea743be806
User Name <user@gmail.com>
2025-10-15 01:30:42 +0300

    modefiy token file

--- backend/utils/createToken.js
+++ backend/utils/createToken.js
@@ -1,13 +1,17 @@
 import jwt from 'jsonwebtoken'
 
 const generateToken = (res , userId) =>{
-  const token = jwt.sign({userId} ,process.env.JWT_SECRET,{expiresIn:'30d'});
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, { expiresIn: '30d' });
 
-  res.cookie('jwt' ,token,{
-    httpOnly :true,
-    secure:process.env.NODE_ENV !== 'development',
-    sameSite:'strict',
-    maxAge: 30*24*60*60*1000
+   const isProduction = process.env.NODE_ENV === 'production';
+
+  
+
+  res.cookie('jwt', token, {
+    httpOnly: true,
+    secure: isProduction, 
+    sameSite: isProduction ? 'None' : 'Lax',
+    maxAge: 30 * 24 * 60 * 60 * 1000
   });
   return token;
 };
