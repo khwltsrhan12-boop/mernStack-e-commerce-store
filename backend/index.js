90f2e1eb05da998cf5b78428fdd8ead9318ea20d
User Name <user@gmail.com>
2025-10-14 21:13:13 +0300

    Update CORS settings in backend and add frontend URL

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-import path from 'path'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
 import connectDB from './config/db.js'
@@ -8,16 +7,32 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import uploadRoutes from './routes/uploadRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import cors from 'cors'
+
+
 
 dotenv.config();
 const port = process.env.PORT || 5000;
 
 connectDB();
 const app = express();
+const allowedOrigins = ['https://mernstack-e-commerce-store.pages.dev', 'http://localhost:3000']; 
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true 
+};
+
 
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
+app.use(cors(corsOptions));
 
 app.use('/api/users' ,userRoutes);
 app.use('/api/category' ,categoryRoutes);
@@ -29,8 +44,8 @@ app.get("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
-const __dirname = path.resolve();
-app.use('/uploads' ,express.static(path.join(__dirname + '/uploads')));
+//const __dirname = path.resolve();
+//app.use('/uploads' ,express.static(path.join(__dirname + '/uploads')));
 
 
 app.listen(port , () => console.log(`Server running on port: ${port}`));
