01a09ac2e72d7b3cafb305edd9e40dc12eb4cfda
User Name <user@gmail.com>
2025-10-14 12:43:36 +0300

    Add fly.toml config files for backend and frontend, move env files

--- frontend/src/redux/constants.js
+++ frontend/src/redux/constants.js
@@ -1,5 +1,5 @@
 
-export const BASE_URL ='';
+export const BASE_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
 export const USERS_URL ='/api/users';
 export const CATEGORY_URL ='/api/category';
 export const PRODUCT_URL ='/api/products';
